1616a40156ff8006e1b93e5e0f4efe9b5d003c61
pincoding <dydtjswkdrns@naver.com>
2024-04-03 01:41:12 +0900

    scroll

--- src/pages/detail/Detail.js
+++ src/pages/detail/Detail.js
@@ -58,13 +58,19 @@ const ButtonBox = styled.div`
   display: ${(props) => props.$Btn};
   padding: 8px 40px;
   position: absolute;
-  right: 120px;
+  right: 0px;
   background-color: #1c1c1c;
   border-radius: 20px;
+  z-index: 100;
   cursor: pointer;
   user-select: none;
+  &:hover {
+    background-color: #00a7f6;
+    cursor: pointer;
+    user-select: none;
+  }
   h1 {
-    font-size: 15px;
+    font-size: 16px;
   }
   @media screen and (max-width: 1024px) {
   }
@@ -151,16 +157,103 @@ const IframeWrap = styled.div`
   transition: 0.5s;
   opacity: ${(props) => props.$OpBg};
 `;
+const Reviews = styled.div`
+  display: block;
+  padding: 8px 30px;
+  position: absolute;
+  right: 120px;
+  background-color: #1c1c1c;
+  border-radius: 20px;
+  cursor: pointer;
+  user-select: none;
+  h1 {
+    font-size: 16px;
+  }
+  @media screen and (max-width: 1024px) {
+  }
+  @media screen and (max-width: 768px) {
+  }
+  @media screen and (max-width: 480px) {
+    padding: 5px 20px;
+    border-radius: 10px;
+    right: 0px;
+    border-radius: 10px;
+    h1 {
+      font-size: 15px;
+    }
+  }
+`;
 
-const DetSec03 = styled.div``;
+const DetSec03 = styled.div`
+  width: 16%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  right: 0;
+  /* background-color: #1d1d1d; */
+  background-color: #111;
+  overflow-y: scroll;
+  opacity: ${(props) => props.$Bg};
+  justify-content: center;
+  display: grid;
+  grid-template-columns: repeat(1);
+  row-gap: 20px;
+`;
+// const DetSec03 = styled.div`
+//   width: 18vw;
+//   height: 100%;
+//   position: fixed;
+//   top: 0;
+//   right: 0;
+//   background-color: #1d1d1d;
+//   display: flex;
+//   overflow-y: scroll;
+//   opacity: ${(props) => props.$Bg};
+// `;
+// const VconWrap = styled.div`
+//   margin-top: 80px;
+//   display: grid;
+//   grid-template-columns: repeat(1, 1fr);
+//   row-gap: 10px;
+//   justify-content: center;
+//   margin: 80px auto;
+// `;
+const BtnClose = styled.div`
+  display: ${(props) => props.$clBtn};
+  padding: 8px 30px;
+  position: absolute;
+  right: 120px;
+  background-color: #1c1c1c;
+  border-radius: 20px;
+  cursor: pointer;
+  user-select: none;
+  h1 {
+    font-size: 16px;
+  }
+  @media screen and (max-width: 1024px) {
+  }
+  @media screen and (max-width: 768px) {
+  }
+  @media screen and (max-width: 480px) {
+    padding: 5px 20px;
+    border-radius: 10px;
+    right: 0px;
+    border-radius: 10px;
+    h1 {
+      font-size: 15px;
+    }
+  }
+`;
 
 export const Detail = () => {
   const { id } = useParams();
   const [data, setData] = useState();
   const [viData, setviData] = useState();
   const [opdata, opData] = useState("0");
+  const [rightData, setRightData] = useState("0");
   // const [widhtData, setwidhtData] = useState("0%");
   const [disData, setDisData] = useState("none");
+  const [textData, setTextData] = useState("none");
 
   const [Loading, setLoading] = useState(true);
   useEffect(() => {
@@ -184,17 +277,25 @@ export const Detail = () => {
 
   const videosHandler = () => {
     opData("1");
-    // setwidhtData("100%");
     setDisData("block");
   };
   const delBtnHandler = () => {
     opData("0");
-    // setwidhtData("0%");
     setDisData("none");
+  };
+
+  const reviewsHandle = () => {
+    setRightData("1");
+    setTextData("block");
+  };
+
+  const closeHandler = () => {
+    setRightData("0");
+    setTextData("none");
   };
   const videoLength = viData && viData.length > 0;
   // console.log(viData && viData[0]?.name);
-  console.log(viData && viData);
+  // console.log(viData && viData);
 
   return (
     <>
@@ -231,12 +332,23 @@ export const Detail = () => {
               </IframeWrap>
             </DetSec01>
             <DetSec02>
-              <ButtonBox
-                onClick={delBtnHandler}
-                $Btn={disData}
-                // $bgSize={widhtData}
-                $OpBg={opdata}
+              <Reviews
+                onClick={reviewsHandle}
+                $clBtn={textData}
+                $Bg={rightData}
               >
+                {console.log(rightData)}
+                <h1>리뷰모음</h1>
+              </Reviews>
+              <BtnClose
+                onClick={closeHandler}
+                $clBtn={textData}
+                $Bg={rightData}
+              >
+                <h1>리뷰닫기</h1>
+              </BtnClose>
+
+              <ButtonBox onClick={delBtnHandler} $Btn={disData} $OpBg={opdata}>
                 <h1>취소</h1>
               </ButtonBox>
               <ConWrap className="conWarp01">
@@ -260,17 +372,22 @@ export const Detail = () => {
             {viData && videoLength ? (
               // viData안에 배열 1번말고 다 가지고온다.
               // 이걸 맵함수로 넣어준다.
-              <DetSec03>
+              <DetSec03 $Bg={rightData}>
+                {/* <VconWrap> */}
                 {viData &&
                   viData.map((data) => (
-                    <iframe
-                      src={`https://www.youtube.com/embed/${data.key}`}
-                      title={data?.name}
-                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                      referrerPolicy="strict-origin-when-cross-origin"
-                      allowFullScreen="fullscreen"
-                    ></iframe>
+                    <div key={data.id}>
+                      <iframe
+                        width="260"
+                        src={`https://www.youtube.com/embed/${data.key}`}
+                        title={data?.name}
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        referrerPolicy="strict-origin-when-cross-origin"
+                        allowFullScreen="fullscreen"
+                      ></iframe>
+                    </div>
                   ))}
+                {/* </VconWrap> */}
               </DetSec03>
             ) : (
               ""
