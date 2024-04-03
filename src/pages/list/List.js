4c9ddb732c306100d27c2678b0d5ceaee1b8bbec
pincoding <dydtjswkdrns@naver.com>
2024-04-04 01:44:10 +0900

    list 작업

--- src/pages/list/List.js
+++ src/pages/list/List.js
@@ -1,27 +1,132 @@
 import { useEffect, useState } from "react";
-import { discover, list02 } from "../../api";
+import { discover } from "../../api";
+import { Link, useParams } from "react-router-dom";
+import { imgURL } from "../../imgurl";
+import styled from "styled-components";
+const Wrap = styled.div`
+  padding: 0px 100px 0px 100px;
+  p {
+    color: #1f77ff;
+  }
+  h1 {
+    font-size: 30px;
+    margin-top: 150px;
+  }
+  @media screen and (max-width: 1024px) {
+    padding: 100px 50px 0px 50px;
+    h1 {
+      font-size: 26px;
+      margin-top: 100px;
+    }
+  }
+  @media screen and (max-width: 768px) {
+    h1 {
+      font-size: 20px;
+      margin-top: 50px;
+    }
+    p {
+      font-size: 14px;
+      /* margin-left: 5px; */
+    }
+    padding: 100px 50px 0px 50px;
+  }
+  @media screen and (max-width: 480px) {
+    padding: 100px 0px 0px 0px;
+    h1 {
+      font-size: 20px;
+      margin-top: 50px;
+      margin-left: 20px;
+      padding-bottom: 20px;
+    }
+    p {
+      margin-top: 8px;
+      margin-left: 15px;
+      font-size: 12px;
+    }
+  }
+`;
+const Section = styled.div`
+  margin-top: 30px;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  row-gap: 20px;
+  column-gap: 20px;
+  @media screen and (max-width: 1024px) {
+  }
+  @media screen and (max-width: 768px) {
+    margin-top: 14px;
+    grid-template-columns: repeat(4, 1fr);
+    row-gap: 10px;
+    column-gap: 10px;
+  }
+  @media screen and (max-width: 480px) {
+    width: 90%;
+    margin: 0 auto;
+    grid-template-columns: repeat(3, 1fr);
+    row-gap: 10px;
+    column-gap: 10px;
+  }
+`;
+const BgImg = styled.div`
+  height: 400px;
+
+  img {
+    height: 100%;
+    object-fit: cover;
+    border-radius: 20px;
+  }
+  @media screen and (max-width: 1024px) {
+    height: 270px;
+  }
+  @media screen and (max-width: 768px) {
+    height: 220px;
+  }
+  @media screen and (max-width: 480px) {
+    height: 160px;
+    img {
+      border-radius: 10px;
+    }
+  }
+`;
 
 export const List = () => {
-  const [cdata02, setcdata02] = useState();
+  const { id } = useParams();
   const [disdata, setDisdata] = useState();
 
   useEffect(() => {
     (async () => {
       try {
-        const list02Data = await list02();
-        const { results } = await discover();
+        const { results } = await discover(id);
         setDisdata(results);
-
-        setcdata02(list02Data);
       } catch (error) {
         console.log(error);
       }
     })();
-  }, []);
+  }, [id]);
 
-  console.log(cdata02);
-  console.log(disdata && disdata.map((data) => data.backdrop_path));
-  console.log(disdata && disdata.map((data) => data.genre_ids));
-
-  return <div>리스트</div>;
+  console.log(disdata);
+  return (
+    <Wrap>
+      <Section>
+        {disdata &&
+          disdata.map((data) => (
+            <Link to={`/detail/${data.id}`} key={data.id}>
+              <BgImg key={data.id}>
+                {data?.poster_path ? (
+                  <img
+                    src={`${imgURL.imgSize300}${data?.poster_path}`}
+                    alt={data?.title}
+                  />
+                ) : (
+                  <img
+                    src={`https://asean.org/wp-content/uploads/2022/07/No-Image-Placeholder.svg.png`}
+                    alt={data?.title}
+                  />
+                )}
+              </BgImg>
+            </Link>
+          ))}
+      </Section>
+    </Wrap>
+  );
 };
